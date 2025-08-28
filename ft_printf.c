d99075f943674aedee3fd9941a4ad9abfb7abe92
Loena Nascimento Da Silva <loda-sil@c2r6p3.42sp.org.br>
2025-08-28 18:36:47 -0300

    feat: add % flag behavior

--- ft_printf.c
+++ ft_printf.c
@@ -3,16 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: loena <loena@student.42.fr>                +#+  +:+       +#+        */
+/*   By: loda-sil <loda-sil@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 18:12:12 by loena             #+#    #+#             */
-/*   Updated: 2025/08/25 23:25:19 by loena            ###   ########.fr       */
+/*   Updated: 2025/08/28 18:32:53 by loda-sil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int	ft_check_format(const char *format, va_list args)
+{
+	if (*format == '%')
+		ft_putchar('%');
+	return (0);
+}
+
 int	ft_printf(const char *format, ...)
 {
-	
+	int 	count;
+	va_list args;
+
+	count = 0;
+	va_start(args, format);
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			count += ft_check_format(*format, args);
+		}
+		else
+			count += ft_putchar(*format);
+		format++;
+	}
+	va_end(args);
+	return (0);
 }
