6633c32a389967c7ab0a7654abf5e30538e8af7a
Loena Nascimento <loenascimentosilva@gmail.com>
2025-09-01 11:52:27 -0300

    feat: add ft_check_format function to handle the ft_printf format

--- ft_printf.c
+++ ft_printf.c
@@ -3,19 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   ft_printf.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: loda-sil <loda-sil@student.42.fr>          +#+  +:+       +#+        */
+/*   By: loena <loena@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/18 18:12:12 by loena             #+#    #+#             */
-/*   Updated: 2025/08/28 18:32:53 by loda-sil         ###   ########.fr       */
+/*   Updated: 2025/08/31 11:53:24 by loena            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_check_format(const char *format, va_list args)
+int	ft_check_format(char format, va_list args)
 {
-	if (*format == '%')
-		ft_putchar('%');
+	unsigned long int ptr_var;
+
+	if (format == '%')
+		return (ft_putchar('%'));
+	else if (format == 'c')
+		return (ft_putchar(va_arg(args, int)));
+	else if (format == 's')
+		return (ft_putstr(va_arg(args, char *)));
+	else if (format == 'd' || format == 'i')
+		return (ft_putnbr(va_arg(args, int)));
+	else if (format == 'u')
+		return (ft_putnbr_unsigned(va_arg(args, unsigned int)));
+	else if (format == 'x')
+		return (ft_puthexa(va_arg(args, unsigned int), "0123456789abcdef"));
+	else if (format == 'X')
+		return (ft_puthexa(va_arg(args, unsigned int), "0123456789ABCDEF"));
+	else if (format == 'p')
+	{
+		ptr_var = va_arg(args, unsigned long int);
+		if (!ptr_var)
+			return (ft_putstr("(nil)"));
+		return (ft_putstr("0x") + ft_puthexa(ptr_var, "0123456789abcdef"));
+	}
 	return (0);
 }
 
@@ -38,5 +59,5 @@ int	ft_printf(const char *format, ...)
 		format++;
 	}
 	va_end(args);
-	return (0);
+	return (count);
 }
